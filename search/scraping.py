8b65bb02e9e04d135a8e1d004f8044fca9319774
iniad <cornforprogramming@gmail.com>
2022-09-30 17:02:18 +0900

    update css selector

--- search/scraping.py
+++ search/scraping.py
@@ -4,6 +4,7 @@ import sys,bs4,requests
 SEARCH_NUM  = "100"
 TIMEOUT     = 10
 HEADER      = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0'}
+# HEADER      = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'}
 
 
 #グーグル検索から検索結果のリストを返す関数
@@ -22,13 +23,15 @@ def search_google(words):
     else:
 
         soup        = bs4.BeautifulSoup(result.content, 'html.parser')
-        links       = soup.select(".rc > div > a")
-        titles      = soup.select(".rc > div > a > h3 > span")
+        # developer toolのelementsタブ 要素右クリから copy-> copy selecter(beautiful soupはセレクターで動くので)
+        links       = soup.select("#rso > div > div > div > div.Z26q7c.UK95Uc.jGGQ5e > div > a")
+        titles      = soup.select("#rso > div > div > div > div.Z26q7c.UK95Uc.jGGQ5e > div > a > h3")
 
         link_list   = [link_tag.get("href") for link_tag in links]
         title_list  = [title_tag.get_text() for title_tag in titles]
 
-
+        
+        print("length {}, {}".format(len(link_list), len(title_list)))
         #タイトルとリンクが不一致の場合、
         if len(link_list) != len(title_list):
             return [],[]
